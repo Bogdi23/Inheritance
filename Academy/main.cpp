6403bb17d5757b2f809a7db0dc91d9ca5d7949a5
bogdant14@mail.ru <bogdant14@mail.ru>
2025-08-22 17:40:04 +0300

    Academy MissionComplete

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -181,8 +181,8 @@ public:
 	{
 		Human::scan(is);
 		char sz_buffer[SPECIALITY_WIDTH + 1] = {};
-		is.read(sz_buffer, SPECIALITY_WIDTH);	//fin.read() читает заданное количество символов из файла, и сохраняет их в NTL (NULL-Terminated Line)
-		
+		is.read(sz_buffer, SPECIALITY_WIDTH);	//fin.read() читает заданное количество сиволов из файла, и сохраняет их в NTL (NULL-Terminated Line);
+
 		//Удаляем лишние пробелы в конце прочитанной строки:
 		for (int i = SPECIALITY_WIDTH - 1; sz_buffer[i] == ' '; i--)sz_buffer[i] = 0;
 
@@ -190,7 +190,7 @@ public:
 		while (sz_buffer[0] == ' ')
 			for (int i = 0; sz_buffer[i]; i++)sz_buffer[i] = sz_buffer[i + 1];
 
-		speciality = sz_buffer; //Сохраняем специальность в соответствующее поле
+		speciality = sz_buffer;	//Сохраняем специальность в соответствующее поле
 
 		is >> group >> rating >> attendance;
 		return is;
@@ -249,7 +249,7 @@ public:
 	{
 		Human::scan(is);
 		char sz_buffer[SPECIALITY_WIDTH + 1] = {};
-		is.read(sz_buffer, SPECIALITY_WIDTH);	//fin.read() читает заданное количество символов из файла, и сохраняет их в NTL (NULL-Terminated Line)
+		is.read(sz_buffer, SPECIALITY_WIDTH);	//fin.read() читает заданное количество сиволов из файла, и сохраняет их в NTL (NULL-Terminated Line);
 
 		//Удаляем лишние пробелы в конце прочитанной строки:
 		for (int i = SPECIALITY_WIDTH - 1; sz_buffer[i] == ' '; i--)sz_buffer[i] = 0;
@@ -258,7 +258,7 @@ public:
 		while (sz_buffer[0] == ' ')
 			for (int i = 0; sz_buffer[i]; i++)sz_buffer[i] = sz_buffer[i + 1];
 
-		speciality = sz_buffer; //Сохраняем специальность в соответствующее поле
+		speciality = sz_buffer;	//Сохраняем специальность в соответствующее поле
 
 		is >> experience;
 		return is;
@@ -364,14 +364,14 @@ Human** Load(const std::string& filename, int& n)
 		cout << "Position " << fin.tellg() << endl;	//Метод tellg() возвращает текущую Get-позицию курсора на чтение. -1 значит eof();
 
 		//4) Загружаем объекты из файла:
-		for (int i = 0; !fin.eof(); )
+		for (int i = 0; i < n; i++)
 		{
 			std::string buffer;
 			std::getline(fin, buffer, ':');
 			if (buffer.size() < 5)continue;
 			group[i] = HumanFactory(buffer);
 			fin >> *group[i];
-			i++;
+			//i++;
 		}
 	}
 	else
@@ -451,5 +451,6 @@ void main()
 	int n = 0;
 	Human** group = Load("group.txt", n);
 	Print(group, n);
+	Save(group, n, "group2.txt");
 	Clear(group, n);
 }
