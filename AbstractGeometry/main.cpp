f46143fec66429f1efee3364765120d174c56c50
bogdant14@mail.ru <bogdant14@mail.ru>
2025-08-25 17:56:06 +0300

    Rectangle and Equilateral_triangle DONE

--- AbstractGeometry/main.cpp
+++ AbstractGeometry/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+п»ї#include <iostream>
 using namespace std;
 
 enum Color
@@ -15,14 +15,14 @@ class Shape
 {
 	Color color;
 public:
-	Shape(Color color) :color(color) {/*Тело конструктора*/}
+	Shape(Color color) :color(color) {/*РўРµР»Рѕ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР°*/}
 	virtual double get_area()const = 0;
 	virtual double get_perimeter()const = 0;
 	virtual void draw()const = 0;
 	virtual void info()const
 	{
-		cout << "Площадь квадрата : " << get_area() << endl;
-		cout << "Периметр квадрата: " << get_perimeter() << endl;
+		cout << "РџР»РѕС‰Р°РґСЊ: " << get_area() << endl;
+		cout << "РџРµСЂРёРјРµС‚СЂ: " << get_perimeter() << endl;
 		draw();
 	}
 };
@@ -66,7 +66,105 @@ public:
 	void info()const override
 	{
 		cout << typeid(*this).name() << endl;
-		cout << "Длина стороны квадрата: " << get_side() << endl;
+		cout << "Р”Р»РёРЅР° СЃС‚РѕСЂРѕРЅС‹ РєРІР°РґСЂР°С‚Р°: " << get_side() << endl;
+		Shape::info();
+	}
+};
+class Rectangle :public Shape
+{
+	double width, height;
+public:
+	Rectangle(double width, double height, Color color) :Shape(color)
+	{
+		set_width(width);
+		set_height(height);
+	}
+	double get_width()const 
+	{ 
+		return width; 
+	}
+	double get_height()const 
+	{ 
+		return height; 
+	}
+	void set_width(double width) 
+	{ 
+		this->width = width; 
+	}
+	void set_height(double height) 
+	{ 
+		this->height = height; 
+	}
+	double get_area()const override 
+	{ 
+		return width * height; 
+	}
+	double get_perimeter()const override 
+	{ 
+		return 2 * (width + height); 
+	}
+	void draw()const override
+	{
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+				cout << "* ";
+			cout << endl;
+		}
+		cout << endl;
+	}
+	void info()const override
+	{
+		cout << typeid(*this).name() << endl;
+		cout << "РЁРёСЂРёРЅР° РїСЂСЏРјРѕСѓРіРѕР»СЊРЅРёРєР°: " << get_width() << endl;
+		cout << "Р’С‹СЃРѕС‚Р° РїСЂСЏРјРѕСѓРіРѕР»СЊРЅРёРєР°: " << get_height() << endl;
+		Shape::info();
+	}
+};
+class Equilateral_triangle :public Shape
+{
+	double side;
+public:
+	Equilateral_triangle(double side, Color color) :Shape(color)
+	{
+		set_side(side);
+	}
+	double get_side()const 
+	{ 
+		return side; 
+	}
+	void set_side(double side) 
+	{ 
+		this->side = side; 
+	}
+	double get_height()const
+	{
+		return (sqrt(3) / 2) * side;
+	}
+	double get_area()const override
+	{
+		return (sqrt(3) / 4) * side * side;
+	}
+	double get_perimeter()const override
+	{
+		return 3 * side;
+	}
+	void draw()const override
+	{
+		int n = (int)side;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n - i; j++) cout << " ";
+			for (int j = 0; j <= i; j++) cout << "* ";
+			cout << endl;
+		}
+		cout << endl;
+	}
+	void info()const override
+	{
+		cout << typeid(*this).name() << endl;
+		cout << "Р”Р»РёРЅР° СЃС‚РѕСЂРѕРЅС‹ СЂР°РІРЅРѕСЃС‚РѕСЂРѕРЅРЅРµРіРѕ С‚СЂРµСѓРіРѕР»СЊРЅРёРєР°: " << get_side() << endl;
+		cout << "Р’С‹СЃРѕС‚Р° СЂР°РІРЅРѕСЃС‚РѕСЂРѕРЅРЅРµРіРѕ С‚СЂРµСѓРіРѕР»СЊРЅРёРєР°: " << get_height() << endl;
 		Shape::info();
 	}
 };
@@ -76,10 +174,18 @@ void main()
 	setlocale(LC_ALL, "");
 	
 	Square square(5, Color::Red);
-	cout << "Длина стороны квадрата: " << square.get_side() << endl;
-	cout << "Площадь квадрата : " << square.get_area() << endl;
-	cout << "Периметр квадрата: " << square.get_perimeter() << endl;
+	cout << "Р”Р»РёРЅР° СЃС‚РѕСЂРѕРЅС‹ РєРІР°РґСЂР°С‚Р°: " << square.get_side() << endl;
+	cout << "РџР»РѕС‰Р°РґСЊ РєРІР°РґСЂР°С‚Р° : " << square.get_area() << endl;
+	cout << "РџРµСЂРёРјРµС‚СЂ РєРІР°РґСЂР°С‚Р°: " << square.get_perimeter() << endl;
 	square.draw();
 	cout << "\n-------------------------------------------\n" << endl;
 	square.info();
+	cout << "\n-------------------------------------------\n" << endl;
+	Rectangle rectangle(7, 3, Color::Blue);
+	rectangle.info();
+	cout << "\n-------------------------------------------\n" << endl;
+	Equilateral_triangle equilateral_triangle(6, Color::Yellow);
+	equilateral_triangle.info();
+	cout << "\n-------------------------------------------\n" << endl;
+
 }
